a90ac869592808709b8a899c3436e26872e9f375
josechaparro <josechaparro.dev@gmail.com>
2022-09-10 12:47:52 -0700

    JS funcationlity added to compute both numbers depending on the operation the user chooses. Reset button also functional

---
+++ js/app.js
@@ -0,0 +1,118 @@
+const numberBtns = document.querySelectorAll('[data-number]');
+const operationBtns = document.querySelectorAll('[data-operation]');
+const equalsBtn = document.querySelector('[data-equals]');
+const deleteBtn = document.querySelector('[data-delete]');
+const resetBtn = document.querySelector('[data-reset]');
+const previousNumberEl = document.querySelector('[data-previous-number]');
+const currentNumberEl = document.querySelector('[data-current-number]');
+
+// Calculator Class
+class Calculator {
+  constructor(previousNumberEl, currentNumberEl) {
+    this.previousNumberEl = previousNumberEl;
+    this.currentNumberEl = currentNumberEl;
+    this.clear();
+  }
+
+  clear() {
+    this.currentNumber = '';
+    this.previousNumber = '';
+    this.operation = undefined;
+  }
+
+  // Appending our number to the current number El
+  appendNum(num) {
+    if (num === '.' && this.currentNumber.includes('.')) return;
+    this.currentNumber = this.currentNumber.toString() + num.toString();
+  }
+  // Choose Operation
+  chooseOperation(operation) {
+    if (this.currentNumber === '') return;
+    if (this.previousNumber !== '') {
+      this.compute();
+    }
+    this.operation = operation;
+    this.previousNumber = this.currentNumber;
+    this.currentNumber = '';
+  }
+
+  // Math functionality
+  compute() {
+    let computation;
+    const previous = parseFloat(this.previousNumber);
+    const current = parseFloat(this.currentNumber);
+    if (isNaN(previous) || isNaN(current)) return;
+    switch (this.operation) {
+      case '+':
+        computation = previous + current;
+        break;
+      case '-':
+        computation = previous - current;
+        break;
+      case '*':
+        computation = previous * current;
+        break;
+      case '/':
+        computation = previous / current;
+        break;
+      default:
+        return;
+    }
+    this.currentNumber = computation;
+    this.operation = undefined;
+    this.previousNumber = '';
+  }
+
+  //Getting the display number
+  getDisplayNumber(num) {
+    const stringNum = num.toString();
+    const integerDigits = parseFloat(stringNum.split('.')[0]);
+    const decimalDigits = stringNum.split('.')[1];
+    let integerDisplay;
+    if (isNaN(integerDigits)) {
+      integerDisplay = '';
+    } else {
+      integerDisplay = integerDigits.toLocaleString('en', { maximumFractionDigits: 0 });
+    }
+    if (decimalDigits != null) {
+      return `${integerDisplay}.${decimalDigits}`;
+    } else {
+      return integerDisplay;
+    }
+  }
+
+  updateDisplay() {
+    this.currentNumberEl.innerText = this.getDisplayNumber(this.currentNumber);
+  }
+}
+
+// Init new Calc
+const calculator = new Calculator(previousNumberEl, currentNumberEl);
+
+// Number Buttons event
+numberBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    calculator.appendNum(btn.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+//Operation Buttons
+operationBtns.forEach((btn) => {
+  btn.addEventListener('click', () => {
+    calculator.chooseOperation(btn.innerText);
+    calculator.updateDisplay();
+  });
+});
+
+// Reset Button
+resetBtn.addEventListener('click', (btn) => {
+  calculator.clear();
+  calculator.updateDisplay();
+});
+
+// Equals Button
+equalsBtn.addEventListener('click', (btn) => {
+  calculator.compute();
+  calculator.updateDisplay();
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,56 @@
 module.exports = {
   content: ['./*.html'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        //Theme 1 ===============
+        //Backgrounds
+        desaturatedBlueOneBG: 'hsl(222, 26%, 31%)',
+        desaturatedBlueTwoBG: 'hsl(223, 31%, 20%)',
+        desaturatedBlueThreeBG: 'hsl(224, 36%, 15%)',
+        //Keys
+        desaturatedBlueOneKey: 'hsl(225, 21%, 49%)',
+        desaturatedBlueTwoKey: 'hsl(224, 28%, 35%)',
+        redOneKey: 'hsl(6, 63%, 50%)',
+        redTwoKey: 'hsl(6, 70%, 34%)',
+        grayishOrangeOne: 'hsl(30, 25%, 89%)',
+        grayishOrangeTwo: 'hsl(28, 16%, 65%)',
+        //Text
+        darkGrayishBlueText: 'hsl(221, 14%, 31%)',
+        light: 'hsl(0, 0, 100%)',
+        //Theme 2 ===============
+        //Backgrounds
+        lightGray: 'hsl(0, 0%, 90%)',
+        grayishRed: 'hsl(0, 5%, 81%)',
+        veryLightGray: 'hsl(0, 0%, 93%)',
+        //Keys
+        darkCyan: 'hsl(185, 42%, 37%)',
+        veryDarkCyan: 'hsl(185, 58%, 25%)',
+        orange: 'hsl(25, 98%, 40%)',
+        darkOrange: 'hsl(25, 99%, 27%)',
+        grayishYellow: 'hsl(45, 7%, 89%)',
+        darkGrayishYellow: 'hsl(35, 11%, 61%)',
+        //Text
+        veryDarkGrayishYellow: 'hsl(60, 10%, 19%)',
+        //Theme 3 ===============
+        //Backgrounds
+        darkVioletBG: 'hsl(268, 75%, 9%)',
+        veryDarkViolet: 'hsl(268, 71%, 12%)',
+        //Keys
+        darkVioletKey: 'hsl(281, 89%, 26%)',
+        vividMagenta: 'hsl(285, 91%, 52%)',
+        pureCyan: 'hsl(176, 100%, 44%)',
+        softCyan: 'hsl(177, 92%, 70%)',
+        darkVioletKey: 'hsl(268, 47%, 21%)',
+        darkMagentaKey: 'hsl(290, 70%, 36%)',
+        //Text
+        lightYellow: 'hsl(52, 100%, 62%)',
+        veryDarkBlueText: 'hsl(198, 20%, 13%)',
+      },
+      fontFamily: {
+        sans: ['League Spartan', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 };
